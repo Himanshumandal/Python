41d602802f46417f325865bfc7f18e282088f0ce
Himanshu Mandal <113971156+Himanshumandal@users.noreply.github.com>
2023-05-02 19:14:51 +0530

    Create comments, escape-seq. and Print.py

---
+++ comments, escape-seq. and Print.py
@@ -0,0 +1,16 @@
+#comment using
+#single comment
+""" this is multiple comment"""
+#escape sequence character
+print("hey i am a boy  and \nthis is me")
+# another escape sequence character
+print("hey i am a \"boy\" and \n this is a me ok")
+
+# print statement
+print("heloo",5,4)
+# Using Separator
+print("usha",3,5,sep=" ")
+
+#using end 
+print("ant",34,54,end="\n")
+print("chill bro")
