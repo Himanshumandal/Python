7a3e3fb1560b4783173436b5ba2205f15b428817
Himanshu Mandal <113971156+Himanshumandal@users.noreply.github.com>
2023-05-03 22:27:11 +0530

    Create Basic-Calculator and Typecasting.py

---
+++ Basic-Calculator and Typecasting.py
@@ -0,0 +1,23 @@
+# Basic Calculator
+
+a=3 
+b=4
+print("The addition=",a+b)
+print("The subtract=",a-b)
+print("The Multiply=",a*b)
+print("The Division=",a/b)
+print("The Modulus=",a%b)
+print("The floorDivison=",a//b)
+print("The expontent=",a**b)
+print("\n")
+#Typecasting
+
+#Explicit
+a="200"
+b="200"
+print("the sum=",int(a)+int(b))
+
+#implicit
+a=10.2
+b=2
+print("the answer",a+b)
