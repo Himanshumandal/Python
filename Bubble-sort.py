385db274423148ea0a2d674fdf7bd07de7243fa6
Himanshu Mandal <113971156+Himanshumandal@users.noreply.github.com>
2023-05-14 23:24:21 +0530

    Update Bubble-sort.py

--- Bubble-sort.py
+++ Bubble-sort.py
@@ -2,7 +2,7 @@
 # Write Python 3 code in this online editor and run it.
 list=[5,4,3,2,1]
 n=len(list)
-#selection sort
+#bubble sort
 for i in range(n):
     for j in range(n-i-1):
         if list[j]>list[j+1]:
